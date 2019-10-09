68f0ef75b3e18a6306507e73652e777c64fafbdf
MrDarkAngel95 <yanohaypetroleo@hotmail.com>
2019-10-09 02:14:26 +0000

    practica7

---
+++ filos.c
@@ -0,0 +1,55 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#define NUM_FILOS 5
+
+pthread_mutex_t tenedores[NUM_FILOS];
+
+void * comer() {
+    sleep(1 + rand()%5);
+}
+
+void * vida(void * id) {
+    long tid;
+    tid = (long)id;
+    srand(tid + time(NULL));
+    while(1) {
+        //piensan
+        printf("Soy el hilo %ld, y estoy pensando... \n", tid);
+        if(tid%2==0) {
+            pthread_mutex_lock(&tenedores[tid]);
+            pthread_mutex_lock(&tenedores[(tid+1)%NUM_FILOS]);
+            //Comer
+            printf("Soy el hilo %ld, y estoy comiendo \n", tid);
+            comer();
+            pthread_mutex_unlock(&tenedores[tid]);
+            pthread_mutex_unlock(&tenedores[(tid+1)%NUM_FILOS]);
+        } else {
+            pthread_mutex_lock(&tenedores[(tid+1)%NUM_FILOS]);
+            pthread_mutex_lock(&tenedores[tid]);
+            //Comer
+            printf("Soy el hilo %ld, y estoy comiendo \n", tid);
+            comer();
+            pthread_mutex_unlock(&tenedores[tid]);
+            pthread_mutex_unlock(&tenedores[(tid+1)%NUM_FILOS]);
+        }
+    }
+}
+
+int main() {
+pthread_t threads[NUM_FILOS];
+   int rc;
+   long t;
+   for(t=0; t<NUM_FILOS; t++){
+       printf("In main: creating thread %ld\n", t);
+       rc = pthread_create(&threads[t], NULL, vida, (void *)t);
+       if (rc){
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          exit(-1);
+       }
+    }
+
+    pthread_exit(NULL);
+}
