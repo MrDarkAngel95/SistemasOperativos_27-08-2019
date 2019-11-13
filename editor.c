e5e6428f80093a2d2d83ea27e5303556dea3c62b
MrDarkAngel95 <yanohaypetroleo@hotmail.com>
2019-11-13 03:23:09 +0000

    practica10

---
+++ editor.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv){
+    char *pattern = argv[1];
+    char *newWord = argv[2];
+    char *fileName = argv[3];
+    char c;
+    char word[100];
+    int i = 0;
+    int fd = open(fileName,O_RDONLY);
+    int fd2 = open("tmp.txt",O_WRONLY | O_CREAT,0666);
+
+    while(read(fd,&c,1)) {
+        if(c != ' ') {
+            word[i++] = c;
+        }else{
+            word[i] = '\0';
+            i = 0;
+            if(!strcmp(word,pattern)) {
+                write(fd2,newWord,strlen(newWord));
+                write(fd2,&c,1);
+            }else{
+                write(fd2,word,strlen(word));
+                write(fd2,&c,1);
+            }
+        }
+    }
+
+    close(fd);
+    word[i] = '\0';
+    if(!strcmp(word,pattern)) {
+        write(fd2,newWord,strlen(newWord));
+        write(fd2,&c,1);
+    }else{
+        write(fd2,word,strlen(word));
+        write(fd2,&c,1);
+    }
+    close(fd2);
+    return 0;
+}
